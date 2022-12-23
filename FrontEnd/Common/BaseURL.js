8f9050d59a763d8a15ab9d30584320b7c4021f84
nadunchanna98 <gancp1218@gmail.com>
2022-12-23 21:15:01 +0530

    add base urls

--- FrontEnd/Common/BaseURL.js
+++ FrontEnd/Common/BaseURL.js
@@ -3,10 +3,14 @@ import { Platform } from "react-native";
 let BASE_URL = "";
 
 if (Platform.OS === "android") {
-    BASE_URL = "http://192.168.8.129:3000/api/v1/" // for android emulator
+    BASE_URL = "https://e-week-app-backend-2.onrender.com/api/v1/" // for android emulator
 } else {
-    BASE_URL = "http://192.168.8.129:3000/api/v1/" // for ios simulator
+    BASE_URL = "https://e-week-app-backend-2.onrender.com/api/v1/" // for ios emulator
 
 }
+
+
+
+
 
 export default BASE_URL;
